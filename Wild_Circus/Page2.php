9a1bfd8d1c21b86a81e19d6298e4a87cd17c994b
Benoit Franjou <benoit.franjou@laposte.net>
2016-02-23 21:39:50 +0100

    Page 2 creation with img file

---
+++ Wild_Circus/Page2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+      <html>
+        <head>
+	      <title>Wild Circus</title>
+	      <link rel="stylesheet" type="text/css" href="CSSwildcircusphp.css">
+	      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Londrina+Shadow">
+        </head>
+
+        <body>
+          <div id="block_page">
+
+          <?php include('fichier PHP/header_index.php'); ?>
+
+          <?php include('fichier PHP/nav_index.php'); ?>
+
+            <div id="block_links">
+              <ul id="Liens_haut_de_page">
+                <li><a href="index.php" id="liens_complement">Reception</a></li>
+                <li><a href="#" id="liens_complement">Contact</a></li>
+                <li><a href="#" id="liens_complement">Forum</a></li>
+                <li><a href="#" id="liens_complement">Photo</a></li>
+              </ul>
+            </div>
+
+             <article>
+              <h2>Fire jugglers</h2>
+
+                <p><em>
+                  This show presents 5 jugglers, who will manipulate and throw different tools, balls, sticks, axes, lantern on fire. They will also blow fire in a certain moment.</em>
+                </p>
+
+            </article>
+
+            <footer id="footer_page2">
+
+              <p>Join us on:</p>
+              <img src="fichier Onglet/twitter.png">
+              <img src="fichier Onglet/facebook.png">
+              <img src="fichier Onglet/vimeo.png">
+              <img src="fichier Onglet/flickr.png">
+            </footer>
+           
+          </div>
+
+        </body>
+      </html>
