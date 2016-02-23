c7044be6712740320214b31f3be45fa664cc5452
Benoit Franjou <benoit.franjou@laposte.net>
2016-02-23 21:45:32 +0100

    creation page 3

---
+++ Wild_Circus/Page3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+      <html>
+        <head>
+	      <title>Wild Circus</title>
+	      <link rel="stylesheet" type="text/css" href="CSSwildcircusphp.css">
+	      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Londrina+Shadow">
+        </head>
+
+        <body>
+          <div id="block_page">
+
+          <?php include('fichier PHP/header_index.php'); ?>
+
+          <?php include('fichier PHP/nav_index.php'); ?>
+
+            <div id="block_links">
+              <ul id="Liens_haut_de_page">
+                <li><a href="index.php" id="liens_complement">Reception</a></li>
+                <li><a href="#" id="liens_complement">Contact</a></li>
+                <li><a href="#" id="liens_complement">Forum</a></li>
+                <li><a href="#" id="liens_complement">Photo</a></li>
+              </ul>
+            </div>
+
+            <article>
+              <h2>The jump of the Lion</h2>
+
+                <p><em>this presentation is composed with different kinds of felins (tigers, panthers, leopard) and the most spectacular lion from south Africa. These animals will jump in to fire circle and apply different orders from there trainers.
+                  
+                </em>
+                
+                </p>
+
+            </article>
+
+            <footer id="footer_page3">
+
+              <p>Join us on:</p>
+              <img src="fichier Onglet/twitter.png">
+              <img src="fichier Onglet/facebook.png">
+              <img src="fichier Onglet/vimeo.png">
+              <img src="fichier Onglet/flickr.png">
+            </footer>
+           
+          </div>
+
+        </body>
+      </html>
