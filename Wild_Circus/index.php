307737e155abbdcfd733552627a6282b5f3d288d
Benoit Franjou <benoit.franjou@laposte.net>
2016-02-23 21:26:22 +0100

    Add index php file

---
+++ Wild_Circus/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+      <html>
+        <head>
+	      <title>Wild Circus</title>
+	      <link rel="stylesheet" type="text/css" href="CSSwildcircusphp.css">
+	      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Londrina+Shadow">
+        </head>
+
+        <body>
+          <div id="block_page">
+
+          <?php include('fichier PHP/header_index.php'); ?>
+
+          <?php include('fichier PHP/nav_index.php'); ?>
+
+           
+
+            <div id="block_links">
+              <ul id="Liens_haut_de_page">
+                <li><a href="index.php" id="liens_complement">Reception</a></li>
+                <li><a href="#" id="liens_complement">Contact</a></li>
+                <li><a href="#" id="liens_complement">Forum</a></li>
+                <li><a href="#" id="liens_complement">Photo</a></li>
+              </ul>
+            </div>
+
+            <article>
+              <h2>welcome to the wild circus web site</h2>
+
+                <p><em>
+                The Wild Circus is an attraction that will garanty you a special entertainment, original shows and diversified performances. Our organization presents lions, tigers, elephants, monkeys , jugglers, magicians, clowns. And this without forgetting there different specialties our tricks.
+                We travel around Europe to present theme and we are waiting for you to discover or Circus. 
+                On the right side you can see the links of our current performences, that will give you there details.</em>
+                </p>
+
+            </article>
+
+            <?php include("fichier PHP/footer_index.php"); ?>
+           
+          </div>
+
+        </body>
+      </html>
