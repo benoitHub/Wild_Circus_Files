0ed20e22dff0014d2f19d6394d747e29d72e2f89
Benoit Franjou <benoit.franjou@laposte.net>
2016-02-23 21:33:50 +0100

    Page 1 creation

---
+++ Wild_Circus/Page1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+      <html>
+        <head>
+	      <title>Wild Circus</title>
+	      <link rel="stylesheet" type="text/css" href="CSSwildcircusphp.css">
+	      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Londrina+Shadow">
+        </head>
+
+        <body>
+          <div id="block_page">
+
+          <?php include('fichier PHP/header_index.php'); ?>
+
+          <?php include('fichier PHP/nav_index.php'); ?>
+
+            <div id="block_links">
+              <ul id="Liens_haut_de_page">
+                <li><a href="index.php" id="liens_complement">Reception</a></li>
+                <li><a href="#" id="liens_complement">Contact</a></li>
+                <li><a href="#" id="liens_complement">Forum</a></li>
+                <li><a href="#" id="liens_complement">Photo</a></li>
+              </ul>
+            </div>
+
+            <article>
+              <h2>Elephante demonstration</h2>
+
+                <p><em>
+                In this presentation you will see two African elephants and one Asian elephant.
+				The show is based on to equilibrating theme on a sphere, making theme manipulating objects and moves, by the skills of our elephant trainner.
+				</em>
+                </p>
+
+            </article>
+
+            <footer id="footer_page1">
+
+              <p>Join us on:</p>
+              <img src="fichier Onglet/twitter.png">
+              <img src="fichier Onglet/facebook.png">
+              <img src="fichier Onglet/vimeo.png">
+              <img src="fichier Onglet/flickr.png">
+            </footer>
+           
+          </div>
+
+        </body>
+      </html>
